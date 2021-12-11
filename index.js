8d3d46c436d6dc0c0ad406f02d4b3b8e679f2328
YTDisPro <95575095+YTDisPro@users.noreply.github.com>
2021-12-11 18:24:44 +0100

    2

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 const { Client, Intents } = require('discord.js');
 const config =require("./config.json")
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const token =require("./token.json")
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const client = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MEMBERS,
+    Intents.FLAGS.GUILD_MESSAGES,
+  
+  ] });
 
-client.on('ready', () => {
+client.on('ready', async () => {
+  const data = new SlashCommandBuilder()
+  .setName('ping')
+  .setDescription('Repond Pong')
+
+  await client.guilds.cache.get("912055021288427610").commands.create(data)
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-const prefix = config.prefix
 
-client.login(config.token);
+const prefix = config.prefix
+//commande basique
+client.on("messageCreate", message => {
+  if(message.author.bot) return;
+  if(message.content.startsWith(prefix + "ping")){
+    message.reply('Pong !')
+  }
+})
+
+//commande / 
+client.on('interactionCreate', async interation => {
+  if(!interation.isCommand()) return;
+
+  if(interation.commandName === 'ping'){
+    interation.reply('!ping')
+  }
+  
+
+})
+client.login(token.token);
